76fc3cd08793971f45e3053e5f07ea2f81b765c4
zjy4fun <zjy4fun@gmail.com>
2023-12-04 01:43:58 +0800

    refactor: update template urls

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -2,10 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const messageApi = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ 
-      "React Simple": "direct:https://github.com/zjy4fun/react-simple-template.git#main",
-      "Vue3 Simple": "direct:https://github.com/zjy4fun/vue3-vite-ts-simple-template.git#main",
-      "Vue3 + Vite + Arco Design": "direct:https://github.com/zjy4fun/arco-vue3-template.git#main",
-      "Vue3 + Vite + Tailwind CSS": "direct:https://github.com/zjy4fun/tailwindcss-vue3-vite-template.git#main",
+      "React": "direct:https://github.com/zjy4fun/react-simple-template.git#main",
+      "Vue3": "direct:https://github.com/zjy4fun/vue3-vite-ts-simple-template.git#main",
+      "React Native": "direct:https://github.com/zjy4fun/react-native-template.git#main",
+      // "Vue3 + Vite + Arco Design": "direct:https://github.com/zjy4fun/arco-vue3-template.git#main",
+      // "Vue3 + Vite + Tailwind CSS": "direct:https://github.com/zjy4fun/tailwindcss-vue3-vite-template.git#main",
   });
 };
 
