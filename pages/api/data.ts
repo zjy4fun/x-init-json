3c41940cc2b51c2bf9e5453ac1c6dd4ac3ddac45
zjy4fun <zjy4fun@gmail.com>
2024-02-05 20:11:04 +0800

    add electron template

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -5,6 +5,7 @@ const messageApi = (req: NextApiRequest, res: NextApiResponse) => {
       "React": "direct:https://github.com/zjy4fun/react-simple-template.git#main",
       "Vue3": "direct:https://github.com/zjy4fun/vue3-vite-ts-simple-template.git#main",
       "React Native": "direct:https://github.com/zjy4fun/react-native-template.git#main",
+      "Electron": "direct:https://github.com/zjy4fun/electron-template.git#main"
       // "Vue3 + Vite + Arco Design": "direct:https://github.com/zjy4fun/arco-vue3-template.git#main",
       // "Vue3 + Vite + Tailwind CSS": "direct:https://github.com/zjy4fun/tailwindcss-vue3-vite-template.git#main",
   });
